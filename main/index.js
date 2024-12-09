0b202213dcef670e9df344cdefda4acb6be7d388
sepi <sepi@code.ir>
2024-12-09 15:56:11 +0330

    added everything

---
+++ main/index.js
@@ -0,0 +1,7 @@
+header = document.getElementById("hp");
+header.onmousemove = function () {
+  hp.innerHTML = "🎃🎃spooky🎃🎃";
+};
+header.onmouseleave = function () {
+  hp.innerHTML = "🎼lilo music🎼";
+};
